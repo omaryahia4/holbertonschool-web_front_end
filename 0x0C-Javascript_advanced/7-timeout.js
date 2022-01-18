01431033e8763d00326190a3e7d43cff33540bb3
omaryahia4 <omar.yahia4444@gmail.com>
2022-01-18 17:19:34 +0100

    timeout

---
+++ 0x0C-Javascript_advanced/7-timeout.js
@@ -0,0 +1,9 @@
+console.log('Start of the execution queue');
+function t() {
+    for(let i=1; i<101;i++) {
+        console.log(i)
+    }
+}
+const myTimeout = setTimeout = (thousand(), 0, 'Final code block to be executed');
+console.log(myTimeout);
+console.log('End of the loop printing')
