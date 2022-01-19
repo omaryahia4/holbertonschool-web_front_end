dfe0cac77be9561b0513432e345ef7809e5299c7
omaryahia4 <omar.yahia4444@gmail.com>
2022-01-19 16:32:05 +0100

    Binding

---
+++ 0x0C-Javascript_advanced/12-room_area.js
@@ -0,0 +1,9 @@
+let roomDimensions = {
+    width: 50,
+    length: 100,
+    getArea: function(){
+        return this.width * this.length;
+    }
+};
+let boundGetArea = roomDimensions.getArea.bind(roomDimensions);
+console.log(boundGetArea());
